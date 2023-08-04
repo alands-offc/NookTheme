dc6b5e704f0c0108e3982ff3a90c55cf1c35ab7a
Angelillo15 <contact@angelillo15.es>
2023-08-04 19:22:32 +0200

    Create SideBar with Children props

--- resources/scripts/components/Sidebar.tsx
+++ resources/scripts/components/Sidebar.tsx
@@ -1,32 +1,14 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { ReactNode } from 'react';
 import '@/assets/css/sidebar.css';
 
-const Sidebar = () => {
+type ParentProps = {
+    children: ReactNode;
+};
+
+export default ({ children }: Omit<ParentProps, 'render'>) => {
     return (
         <>
-            <div className='sidebar'>
-                <NavLink to='/'>
-                    Home <i className='bx bxs-home-heart' />
-                </NavLink>
-
-                <NavLink to='/resources'>
-                    Resources <i className='bx bx-list-ul' />
-                </NavLink>
-
-                <NavLink className='router-link-active' to='' v-if='isEditor'>
-                    Editor <i className='bx bx-edit-alt' />
-                </NavLink>
-
-                <a href='https://github.com/Angelillo15' target='_blank' rel='noreferrer'>
-                    GitHub <i className='bx bxl-github' />
-                </a>
-                <a href='https://discord.nookure.com' target='_blank' rel='noreferrer'>
-                    Discord <i className='bx bxl-discord-alt' />
-                </a>
-            </div>
+            <div className='sidebar'>{children}</div>
         </>
     );
 };
-
-export default Sidebar;
